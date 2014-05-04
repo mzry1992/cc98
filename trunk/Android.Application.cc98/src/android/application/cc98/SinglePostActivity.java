181d327f534627c65df8f3e88e000b577b29c63f
lcr.71107320@gmail.com <lcr.71107320@gmail.com@3c59b369-1c2a-dc98-6d8b-44e5ebef8c45>
2014-05-04 14:35:54 +0000

    Add error handle

--- trunk/Android.Application.cc98/src/android/application/cc98/SinglePostActivity.java
+++ trunk/Android.Application.cc98/src/android/application/cc98/SinglePostActivity.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.Activity;
+import android.application.cc98.network.LeafBoardTask;
 import android.application.cc98.network.SinglePostTask;
 import android.application.cc98.network.UserInfoUtil;
 import android.application.cc98.view.Utility;
@@ -17,104 +18,127 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
-public class SinglePostActivity extends Activity implements OnClickListener, GetWebPageInterface{
+public class SinglePostActivity extends LoadWebPageActivity implements
+		OnClickListener {
 
-	// temporary variables to store data from HTML, updated when loading following posts
+	// temporary variables to store data from HTML, updated when loading
+	// following posts
 	private ArrayList<String> authors;
 	private ArrayList<String> contents;
 	private ArrayList<String> timestamps;
-	
+
 	// global variables to store data of the topic list
 	private ArrayList<String> postInfo;
-	
+
 	// variables to record post information
 	private int postCount, pageCount, displayedPage = 0;
 	private String cookie, serverName, postTitle, postUrl;
-	
+
 	// UI and data
 	private Button postMoreBtn;
 	private SimpleAdapter listItemAdapter;
 	private ArrayList<HashMap<String, String>> displist;
 	
+	//mark status
+	private boolean firstPageLoadSucess = false;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.single_post);
-		postMoreBtn = (Button)this.findViewById(R.id.singlePostMoreButton);
-		postMoreBtn.setOnClickListener(this);
 		
 		Intent intent = getIntent();
-        postUrl = intent.getStringExtra(this.getString(R.string.postUrl));
-        StringBuilder sb = new StringBuilder();
-        sb.append(postUrl); sb.append("&star=");
-        postUrl = sb.toString();
-        
-		cookie = UserInfoUtil.GetCookieInfo(this);
+		postUrl = intent.getStringExtra(this.getString(R.string.postUrl));
+		StringBuilder sb = new StringBuilder();
+		sb.append(postUrl);
+		sb.append("&star=");
+		postUrl = sb.toString();
+
 		serverName = this.getString(R.string.serverName);
+	}
+	
+	@Override
+	public void loadPage() {
 		new SinglePostTask(this, serverName).execute(cookie, postUrl + "1");
 	}
-	
+
 	@Override
-	public void getWebPagePreProgress() {
-		if (displayedPage == 0 ) {
-			// set layout invisible
-			LinearLayout layout = (LinearLayout)this.findViewById(R.id.singlePostLayout);
-			layout.setVisibility(View.INVISIBLE);
-		}
-	}
-	
-	@Override
-	public void getWebPageProgressUpdate() {
+	public void loadPageSucess(Object output) {
 		
+		ArrayList<ArrayList<String>> outputs = (ArrayList<ArrayList<String>>) output;
+		ArrayList<String> status = outputs.get(0);
+		
+		if (status.get(0).equals("1")) {
+			setContentView(R.layout.single_post);
+			postMoreBtn = (Button) this.findViewById(R.id.singlePostMoreButton);
+			postMoreBtn.setOnClickListener(this);
+		}
+
+		fillContent(output);
 	}
 	
-	@Override
-	public void getWebPagePostProgress(Object outputRes) {
-		ArrayList<ArrayList<String>> outputs = (ArrayList<ArrayList<String>>)outputRes;
+	public int getStatusCode(Object outputRes) {
+		ArrayList<ArrayList<String>> outputs = (ArrayList<ArrayList<String>>) outputRes;
+		ArrayList<String> status = outputs.get(0);
+		int statusCode = Integer.parseInt(status.get(0));
+		if (statusCode == 1 || firstPageLoadSucess)
+			statusCode = 3;
+		return statusCode;
+	}
+
+	public String getErrorMessage(Object outputs) {
+		return "";
+	}
+
+
+	public void fillContent(Object outputRes) {
+		
+		ArrayList<ArrayList<String>> outputs = (ArrayList<ArrayList<String>>) outputRes;
 		ArrayList<String> status = outputs.get(0);
 
 		// first post page loading finish
 		if (status.get(0).equals("1")) {
+
+			firstPageLoadSucess = true;
 			
 			postInfo = outputs.get(1);
-			
+
 			postCount = Integer.parseInt(postInfo.get(0));
 			pageCount = Integer.parseInt(postInfo.get(1));
 			postTitle = postInfo.get(2);
 			displayedPage = 1;
-			
+
 			authors = outputs.get(2);
 			contents = outputs.get(3);
 			timestamps = outputs.get(4);
-			
+
 			// set single post UI
 			setSinglePostUI();
-			
+
 			// set layout visible
-			LinearLayout layout = (LinearLayout)this.findViewById(R.id.singlePostLayout);
+			LinearLayout layout = (LinearLayout) this
+					.findViewById(R.id.singlePostLayout);
 			layout.setVisibility(View.VISIBLE);
 		}
 		// following post page loading finish
 		else if (status.get(0).equals("3")) {
-			
+
 			authors = outputs.get(2);
 			contents = outputs.get(3);
 			timestamps = outputs.get(4);
-			
+
 			// update UI for following posts
 			updateSinglePostUI();
-		}
-		else {
-		}
+		} 
 	}
-	
+
 	private void setSinglePostUI() {
-		TextView postTitleTv = (TextView)this.findViewById(R.id.singlePostTitle);
+		TextView postTitleTv = (TextView) this
+				.findViewById(R.id.singlePostTitle);
 		postTitleTv.setText(postTitle);
-		
-		TextView postInfoTv = (TextView)this.findViewById(R.id.singlePostInfo);
+
+		TextView postInfoTv = (TextView) this.findViewById(R.id.singlePostInfo);
 		postInfoTv.setText("»ØÌû×ÜÊý£º" + (postCount - 1));
-		
+
 		if (displayedPage < pageCount)
 			postMoreBtn.setText(this.getString(R.string.morePostText));
 		else
@@ -126,7 +150,7 @@ public class SinglePostActivity extends Activity implements OnClickListener, Get
 		String postContentText = this.getString(R.string.postContentText);
 		String postTimestampText = this.getString(R.string.postTimestampText);
 		displist = new ArrayList<HashMap<String, String>>();
-		
+
 		for (int i = 0; i < authors.size(); i++) {
 			HashMap<String, String> map = new HashMap<String, String>();
 			map.put(postFloorText, " " + i + "F ");
@@ -135,28 +159,33 @@ public class SinglePostActivity extends Activity implements OnClickListener, Get
 			map.put(postContentText, contents.get(i));
 			displist.add(map);
 		}
-		
-		listItemAdapter = new SimpleAdapter(this,
-        		displist,
-        		R.layout.single_post_list_item,	//	ListItem XML implementation
-                new String[] {	postFloorText, postAuthorText, 
-        						postTimestampText, postContentText}, // dynamic array and ListItem correspondings        
-                new int[] {	R.id.postFloorText, R.id.postAuthorText,
-        					R.id.postTimestampText, R.id.postContentText}); // ListItem XML's two TextView ID
-        
-        // set custom list view
-     	ListView postLv = (ListView)this.findViewById(R.id.singlePostList);
-     	postLv.setAdapter(listItemAdapter);
-     	Utility.setListViewHeightBasedOnChildren(postLv);
+
+		listItemAdapter = new SimpleAdapter(this, displist,
+				R.layout.single_post_list_item, // ListItem XML implementation
+				new String[] { postFloorText, postAuthorText,
+						postTimestampText, postContentText }, // dynamic array
+																// and ListItem
+																// correspondings
+				new int[] { R.id.postFloorText, R.id.postAuthorText,
+						R.id.postTimestampText, R.id.postContentText }); // ListItem
+																			// XML's
+																			// two
+																			// TextView
+																			// ID
+
+		// set custom list view
+		ListView postLv = (ListView) this.findViewById(R.id.singlePostList);
+		postLv.setAdapter(listItemAdapter);
+		Utility.setListViewHeightBasedOnChildren(postLv);
 	}
-	
+
 	private void updateSinglePostUI() {
 		// set data
 		String postFloorText = this.getString(R.string.postFLoorText);
 		String postAuthorText = this.getString(R.string.postAuthorText);
 		String postContentText = this.getString(R.string.postContentText);
 		String postTimestampText = this.getString(R.string.postTimestampText);
-		
+
 		int baseIndex = (displayedPage - 1) * 10;
 		for (int i = 0; i < authors.size(); i++) {
 			HashMap<String, String> map = new HashMap<String, String>();
@@ -167,28 +196,32 @@ public class SinglePostActivity extends Activity implements OnClickListener, Get
 			displist.add(map);
 		}
 		// inform adapter to update UI
-		listItemAdapter.notifyDataSetChanged(); 
-		
+		listItemAdapter.notifyDataSetChanged();
+
 		if (displayedPage < pageCount)
 			postMoreBtn.setText(this.getString(R.string.morePostText));
 		else
 			postMoreBtn.setText(this.getString(R.string.noMorePostText));
 	}
-	
+
 	public void onClick(View v) {
-    	switch (v.getId()) {
-    	case R.id.singlePostMoreButton:
-    		//System.out.println("Click singlePostMoreButton");
+		switch (v.getId()) {
+		case R.id.singlePostMoreButton:
+			// System.out.println("Click singlePostMoreButton");
 			if (displayedPage >= pageCount)
 				postMoreBtn.setText(this.getString(R.string.noMorePostText));
 			else {
 				displayedPage++;
-				String followingPostUrl = postUrl + Integer.toString(displayedPage);
-				System.out.println("Loading follow post URL: " + followingPostUrl);
-				postMoreBtn.setText(this.getString(R.string.loadingMorePostText));
-				new SinglePostTask(this, serverName).execute(cookie, followingPostUrl);
+				String followingPostUrl = postUrl
+						+ Integer.toString(displayedPage);
+				System.out.println("Loading follow post URL: "
+						+ followingPostUrl);
+				postMoreBtn.setText(this
+						.getString(R.string.loadingMorePostText));
+				new SinglePostTask(this, serverName).execute(cookie,
+						followingPostUrl);
 			}
 			break;
-    	}
+		}
 	}
 }
